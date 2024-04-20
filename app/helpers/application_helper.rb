b8df0e1c1764d247a88c48a4c1979f56f99b371e
Mads Sobering <madisonsobering@gmail.com>
2024-04-19 19:12:15 -0500

    Created a helper accessible by all models to check for blank values and return either the value, or a 'not available' message.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Check for blank value, and return "Not Available" if blank.
+  def pretty_null_display(table_value)
+    if table_value.is_a?(Integer) && table_value > 0 then
+      table_value
+    elsif !table_value.blank?
+      strip_tags(table_value)
+    else
+      "Not Available"
+    end
+  end
 end
