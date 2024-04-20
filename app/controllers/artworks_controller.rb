60cef6c7365ec0202fe37aed8f60cccd82718daf
Mads Sobering <madisonsobering@gmail.com>
2024-04-20 17:26:52 -0500

    4.2 Added to the search action to make a more complex query to filter for categories.

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -10,9 +10,15 @@ class ArtworksController < ApplicationController
   # Action for searching.
   def search
     @query = params[:q]
+    @chosen_category = params[:category_sort]
 
-    # Query for artwork title partial matches while avoiding SQL injection.
-    @artworks = Artwork.where('title LIKE ?', "%#{@query}%").page(params[:page])
+    if params[:category_sort].blank?
+      # Query for artwork title partial matches while avoiding SQL injection.
+      @artworks = Artwork.where('title LIKE ?', "%#{@query}%").page(params[:page])
+      @chosen_category = "any"
+    else
+      @artworks = Artwork.where('artworks.title LIKE ?', "%#{@query}%").includes(:categories).where('categories.title = ?', @chosen_category).references(:categories).page(params[:page])
+    end
 
     # Return a count of all artworks.
     @count = @artworks.total_count
