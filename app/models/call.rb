220638a0a83f3ade06d455a08be2640def5af165
Mads Sobering <madisonsobering@gmail.com>
2025-01-29 17:57:39 -0600

    Migrated database and added associations to models.

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,3 +1,4 @@
 class Call < ApplicationRecord
   belongs_to :neighbourhood
+  has_and_belongs_to_many :units
 end
---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2025_01_29_225435) do
+  create_table "calls", force: :cascade do |t|
+    t.datetime "call_time"
+    t.datetime "closed_time"
+    t.string "incident_type"
+    t.boolean "vehicle_crash"
+    t.text "ward"
+    t.integer "neighbourhood_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["neighbourhood_id"], name: "index_calls_on_neighbourhood_id"
+  end
+
+  create_table "calls_units", id: false, force: :cascade do |t|
+    t.integer "call_id", null: false
+    t.integer "unit_id", null: false
+  end
+
+  create_table "neighbourhoods", force: :cascade do |t|
+    t.string "name"
+    t.text "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "calls", "neighbourhoods"
+end
