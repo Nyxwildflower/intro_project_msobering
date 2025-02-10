e6581394af2a182f3de9432347aea4b1b1d94c87
Mads Sobering <madisonsobering@gmail.com>
2025-02-09 21:13:00 -0600

    Added a route and controller action for the Call show view.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -2,4 +2,8 @@ class CallsController < ApplicationController
   def index
     @calls = Call.includes(:neighbourhood).includes(:units).all
   end
+
+  def show
+    @call = Call.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "calls#index"
+
+  get '/calls/:id', to: 'calls#show', as: 'call'
 end
