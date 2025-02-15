cbd5372819c6df3257ff1608a45ae3a43938353a
Mads Sobering <madisonsobering@gmail.com>
2025-02-15 14:12:36 -0600

    Added a search form to the menu that searches through incident types, and a filter for the calls returned on the controller.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,15 @@
 class CallsController < ApplicationController
   def index
     @calls = Call.page(params[:page]).includes(:neighbourhood).includes(:units)
+
+    # If parameters are sent from the search bar, and they're not blank,
+    # the normal index variable is filtered down further by the search query.
+    # The search is for incident type.
+    if params[:search_by_incident].strip != ""
+      @query = params[:search_by_incident]
+
+      @calls = @calls.where("incident_type LIKE ?", "%#{params[:search_by_incident]}%").page(params[:page]).includes(:neighbourhood).includes(:units)
+    end
   end
 
   def show
