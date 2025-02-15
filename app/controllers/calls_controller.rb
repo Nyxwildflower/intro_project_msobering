4a21ac6f74a3c10307fd04bb30822d8a4b6caaa0
Mads Sobering <madisonsobering@gmail.com>
2025-02-15 17:12:27 -0600

    Added a line to save a count of all returned search results using Kaminari's method for counting paginated records.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -5,10 +5,12 @@ class CallsController < ApplicationController
     # If parameters are sent from the search bar, and they're not blank,
     # the normal index variable is filtered down further by the search query.
     # The search is for incident type.
-    if params[:search_by_incident].strip != ""
+    if params[:search_by_incident] && params[:search_by_incident].strip != ""
       @query = params[:search_by_incident]
 
       @calls = @calls.where("incident_type LIKE ?", "%#{params[:search_by_incident]}%").page(params[:page]).includes(:neighbourhood).includes(:units)
+      # Use kaminari's method to get the total count of paginated results.
+      @count = @calls.total_count
     end
   end
 
