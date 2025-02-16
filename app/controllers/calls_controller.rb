5bd99585e583e9727a4d56f7de956640044def21
Mads Sobering <madisonsobering@gmail.com>
2025-02-16 15:22:52 -0600

    Added another filter for neighbourhoods so a user can either search for one or both values.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -8,8 +8,18 @@ class CallsController < ApplicationController
     if params[:search_by_incident] && params[:search_by_incident].strip != ""
       @query = params[:search_by_incident]
 
-      @calls = @calls.where("incident_type LIKE ?", "%#{params[:search_by_incident]}%").page(params[:page]).includes(:neighbourhood).includes(:units)
+      @calls = @calls.where("incident_type LIKE ?", "%#{params[:search_by_incident]}%").page(params[:page])
       # Use kaminari's method to get the total count of paginated results.
+      @count = @calls.total_count
+    end
+
+    if params[:search_by_neighbourhood] && !params[:search_by_neighbourhood].blank?
+      @filter_query = params[:search_by_neighbourhood]
+
+      # Filter current call variable further by neighbourhood name.
+      @calls = @calls.where(neighbourhood: {name: params[:search_by_neighbourhood]}).page(params[:page])
+
+      # Update count.
       @count = @calls.total_count
     end
   end
